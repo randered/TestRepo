a6061f1641a5a4c7cd54a811c4e20ab48d90318d
randered <68559794+randered@users.noreply.github.com>
2021-05-20 23:10:14 +0300

    New Exersices

--- src/SetAndMapEx1.java
+++ src/SetAndMapEx1.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class SetAndMapEx1 {
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class SetAndMapEx1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedHashSet<String> usernames = new LinkedHashSet<>();
+        int numberOfEntries = Integer.parseInt(scanner.nextLine());
+        while (numberOfEntries!=0){
+            String entry = scanner.nextLine();
+            if(!usernames.contains(entry)){
+             usernames.add(entry);
+            }
+            numberOfEntries--;
+        }
+        for (String user: usernames) {
+            System.out.println(user);
+        }
+
+
+    }
 }
